cca35a1f2d07d7fb5ce768a026539cac7356ec2d
Jhon Dayron Orozco <Jhon.orozco@javerianacali.edu.co>
2022-10-28 16:19:53 -0500

    ADD Objetos literales

---
+++ src/typescript/ObjetosLiterales.tsx
@@ -0,0 +1,32 @@
+
+interface Persona {
+    nombreCompleto: string;
+    edad: number;
+    direccion: Direccion;
+}
+
+//Se usa cuando se tiene un objeto anidado
+interface Direccion{
+    pais: string;
+    casaNo: number;
+}
+
+export const ObjetosLiterales = () => {
+
+    const persona: Persona = {
+        nombreCompleto: 'Jhon',
+        edad: 23,
+        direccion:{
+            pais: 'Colombia',
+            casaNo: 49
+        }
+    }
+
+
+  return (
+    <>
+        <h3>Objetos Literales</h3>
+        { JSON.stringify(persona, null, 2)}
+    </>
+  )
+}
