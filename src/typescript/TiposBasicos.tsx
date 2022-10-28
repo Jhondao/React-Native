bf7b27a36a564ed2c9c9b7457f3886d4b698952c
Jhon Dayron Orozco <Jhon.orozco@javerianacali.edu.co>
2022-10-28 16:03:53 -0500

    Tipos basicos

--- src/typescript/TiposBasicos.tsx
+++ src/typescript/TiposBasicos.tsx
@@ -1,8 +1,24 @@
 
 export const TiposBasicos = () => {
+
+    const nombre  = "Jhon";
+    const edad = 35;
+    const estaActivo:boolean = false;
+
+    //Arreglo de stings 
+    const poderes: string[] = [];
+
+    poderes.push("Hola mundo");
+    poderes.push("Hola bebe");
+    
+    
+
   return (
     <>
         <h3>Tipos básicos</h3>
+        { nombre }, { edad }, {(estaActivo) ? 'Actvio' : 'no activo'}
+        <br />
+        { poderes.join(' , ')}
     </>
   )
 }
