aa237ae16c821f8e83abbda983387667caa614cb
Jhon Orozco <jhondao.desarrollo@hotmaile.com>
2022-11-01 21:35:16 -0500

    Mostrando los usuarios consumidos de la API

--- src/components/Usuarios.tsx
+++ src/components/Usuarios.tsx
@@ -10,12 +10,26 @@ export const Usuarios = () => {
       //Llamado al API
       reqResAPI.get<ReqResListado>('/users')
             .then( resp => {
-                console.log(resp.data.data);
+                setUsuarios(resp.data.data)
             })
             .catch(console.log)
     }, [])
     
 
+    const renderItem = ({id, first_name, last_name, email, avatar}: Usuario) => {
+        return(
+            <tr key= {id.toString()} >
+                <td> 
+                    <img src={avatar} alt={first_name} style={{
+                        width: 50,
+                        borderRadius: 100
+                    }} />
+                </td>
+                <td> {first_name} {last_name} </td>
+                <td> {email} </td>
+            </tr>
+        )
+    }
 
   return (
     <>
@@ -29,7 +43,9 @@ export const Usuarios = () => {
                 </tr>
             </thead>
             <tbody>
-
+                {
+                    usuarios.map(usuario => renderItem(usuario))
+                }
             </tbody>
         </table>
     </>
