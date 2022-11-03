34a81a3cca1989fd6243ab7b56451c13b36b499a
Jhon Dayron Orozco <Jhon.orozco@javerianacali.edu.co>
2022-11-03 17:04:16 -0500

    [ADD] Custom Hook - useForm

--- src/components/Formularios.tsx
+++ src/components/Formularios.tsx
@@ -1,29 +1,20 @@
-import { useState } from 'react';
+import { useForm } from '../hooks/useForm';
 
 export const Formularios = () => {
 
-    const [formulario, setFormulario] = useState({
-        email: 'test@test.com',
-        password: '123456'
+    const {state, email, password, onChange} = useForm({
+        email: '',
+        password: ''
     });
-
-
-    const onChange = (value:string, campo:string) => {
-        setFormulario({
-            ...formulario,
-            [campo]: value
-        })
-
-    }
-
+    
   return (
     <>
         <h3>Formularios</h3>
-        <input type="text" className="form-control" placeholder="Email" value={ formulario.email }  onChange={ ({target}) => onChange( target.value, 'email' ) } />
-        <input type="text" className="form-control mt-2 mb-2" placeholder="Password" value={ formulario.password } onChange={ ({target}) => onChange( target.value, 'password' ) }/>
+        <input type="text" className="form-control" placeholder="Email" value={ email }  onChange={ ({target}) => onChange( target.value, 'email' ) } />
+        <input type="text" className="form-control mt-2 mb-2" placeholder="Password" value={ password } onChange={ ({target}) => onChange( target.value, 'password' ) }/>
 
     <code>
-        <pre> {JSON.stringify(formulario, null, 2)} </pre>
+        <pre> {JSON.stringify(state, null, 2)} </pre>
     </code>
     
     </>
